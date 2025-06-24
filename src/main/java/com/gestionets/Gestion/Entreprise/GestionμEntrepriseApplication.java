02851793a5e3c6bf8f4cb92cfa35bcd07f31f2aa
hama <bouallagui.mohamed03@gmail.com>
2025-06-24 16:27:55 +0200

    initisalisation du projet avec uniquement les dependances

---
+++ src/main/java/com/gestionets/Gestion/Entreprise/GestionμEntrepriseApplication.java
@@ -0,0 +1,13 @@
+package com.gestionets.Gestion.Entreprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionμEntrepriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionμEntrepriseApplication.class, args);
+	}
+
+}
